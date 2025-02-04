cf602d63d779830b6fe2dede27e21296f9b4f24e
Dimplereddy28 <Dimplereddy.yalamuri@gmail.com>
2025-02-04 13:03:44 +0530

    pushing react files

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// function App() {
+  //let ele = React.createElement("div" , {className:"APP"}),
+    //React.createElement("h1",null,"Header")
+  //   //)
+  //   let username = "Akshitha";
+  // return (
+    //<div className="App">
+      //<h1>Header</h1>
+      
+    //</div>
+    // <section>
+    //   <h1> ONE JSx element</h1>
+    //   <p>lorm3</p>
+    // </section>
+    // <div>
+      // <h1>{username}</h1>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // </div>
+    // import React from 'react'
+
+    // const 
+import React  from 'react';
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+import Sidebar1 from './components/Sidebar1'
+import Sidebar2 from './components/Sidebar2'
+import footer from './components/footer'
+const App = () => {
+    return (
+        <div className='app'>
+            <Navbar/>
+            <Main/>
+            <div className='sidebar'>
+            <sidebar1/>
+            <sidebar2/>
+            </div>
+            <footer/>
+        </div>
+    )
 }
 
 export default App;
+
+//   );
+// }
+
+// export default App;

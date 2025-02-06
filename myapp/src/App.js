250b69285cd5810fba8cb6d2f4497ac2cffe6306
Dimplereddy28 <Dimplereddy.yalamuri@gmail.com>
2025-02-06 12:56:45 +0530

    pushing the files

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -19,29 +19,66 @@
     // import React from 'react'
 
     // const 
-import React  from 'react';
-import Navbar from './components/Navbar'
-import Main from './components/Main'
-import Sidebar1 from './components/Sidebar1'
-import Sidebar2 from './components/Sidebar2'
-import footer from './components/footer'
-const App = () => {
-    return (
-        <div className='app'>
-            <Navbar/>
-            <Main/>
-            <div className='sidebar'>
-            <sidebar1/>
-            <sidebar2/>
-            </div>
-            <footer/>
-        </div>
-    )
-}
+// import React  from 'react';
+// import Navbar from './components/Navbar'
+// import Main from './components/Main'
+// import Sidebar1 from './components/Sidebar1'
+// import Sidebar2 from './components/Sidebar2'
+// import footer from './components/footer'
+// const App = () => {
+//     return (
+//         <div className='app'>
+//             <Navbar/>
+//             <Main/>
+//             <div className='sidebar'>
+//             <sidebar1/>
+//             <sidebar2/>
+//             </div>
+//             <footer/>
+//         </div>
 
-export default App;
+//     )
+
+// export default App;
 
 //   );
 // }
 
 // export default App;
+// import React, { Component } from 'react'
+// import CBCPropEx from './propsex/CBCPropEx'
+//        <div>
+//         {/* <CBCPropEx */}
+//         username="DimpleReddy"
+//         age={20}
+//         hobbies={["Playing","reading books","sleeping"]}
+//         address={{city:"Palnadu",area:"piduguralla"}}
+//         sendFun={function(){alert("hi i am DimpleReddy")}}
+//         /> *
+
+
+//           <FBCPropEx
+//           username="Abhinay"
+//           isMarried={true}
+//           hobbies={["chatting","Reading"]}
+//           />
+//       </div>
+//     )
+//   }
+// }
+// //
+// import React from 'react'
+// import PropChild
+
+import React from 'react'
+import CBCStateEx from './stateexample/CBCStateEx'
+
+const App = () => {
+  return (
+    <div>
+      <CBCStateEx></CBCStateEx>
+    </div>
+  )
+}
+
+export default App.js;
